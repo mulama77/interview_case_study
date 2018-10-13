db0189482b5b2b2abb85cc199b8a0ad6fc47ace7
mulama77 <kmmulama63@gmail.com>
2018-10-13 16:37:58 +0300

    added the correct wine photos

--- interview-case-study/src/main/java/com/backend/ecommerce/EcommerceApplication.java
+++ interview-case-study/src/main/java/com/backend/ecommerce/EcommerceApplication.java
@@ -17,15 +17,15 @@ public class EcommerceApplication {
     @Bean
     CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.save(new Product(1L, "Breideker", 17.90, "http://placehold.it/200x100"));
-            productService.save(new Product(2L, "Chardonnay", 18.90, "http://placehold.it/200x100"));
-            productService.save(new Product(3L, "Gewurztraminer", 22.90, "http://placehold.it/200x100"));
-            productService.save(new Product(4L, "Hukapapa Dessert Riesling", 21.90, "http://placehold.it/200x100"));
-            productService.save(new Product(5L, "Late Harvest", 21.90, "http://placehold.it/200x100"));
-            productService.save(new Product(6L, "Mirumiru (TM) Reserve", 28.90, "http://placehold.it/200x100"));
-            productService.save(new Product(7L, "Mirumiru (TM) Rose", 34.00, "http://placehold.it/200x100"));
-            productService.save(new Product(8L, "Pinota Gris", 18.90, "http://placehold.it/200x100"));
-            productService.save(new Product(9L, "Pinota Noir", 28.90, "http://placehold.it/200x100"));
+            productService.save(new Product(1L, "Breideker", 17.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/09riesling.png"));
+            productService.save(new Product(2L, "Chardonnay", 18.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/09riesling_v.png"));
+            productService.save(new Product(3L, "Gewurztraminer", 22.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/Breidecker_small.jpg"));
+            productService.save(new Product(4L, "Hukapapa Dessert Riesling", 21.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/Hukapapa_v.png"));
+            productService.save(new Product(5L, "Late Harvest", 21.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/Hunters_Gewurztraminer_NV_-_webv.png"));
+            productService.save(new Product(6L, "Mirumiru (TM) Reserve", 28.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/Hunters_Late_Harvest_Sauvignon-Blanc_NV_-_webv.png"));
+            productService.save(new Product(7L, "Mirumiru (TM) Rose", 34.00, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/chardonnay_v_1.png"));
+            productService.save(new Product(8L, "Pinota Gris", 18.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/hunters_mirumiru_non_vintage_PNG_v3.png"));
+            productService.save(new Product(9L, "Pinota Noir", 28.90, "https://github.com/mulama77/angularjs-spring/blob/master/wine-bottles/pinotnoir_small_v_1.png"));
         };
     }
 }
